a5290ecc28e9f62a648088ef072be3b76606cae9
DrMax <tehnolog3ooo@yandex.ru>
2026-04-18 21:02:09 +0300

    Add Tooltip ForceMobileInputInEditor

--- Assets/Scripts/App/ProjectInstaller.cs
+++ Assets/Scripts/App/ProjectInstaller.cs
@@ -9,8 +9,9 @@ namespace Asteroids.App
 {
     public class ProjectInstaller : MonoInstaller
     {
-        [Header("Debug")]
-        public bool ForceMobileInputInEditor = true;
+        [Header("Debug Controls")]
+        [Tooltip("Поставь галочку, чтобы в Unity Editor появился мобильный джойстик")]
+        public bool ForceMobileInputInEditor = false;
 
         public override void InstallBindings()
         {
@@ -22,9 +23,7 @@ namespace Asteroids.App
             Container.Bind<IConfigProvider>().To<ConfigProvider>().AsSingle();
             Container.Bind<GameStateMachine>().AsSingle();
             Container.Bind<LeaderboardService>().AsSingle();
-            
             Container.BindInterfacesAndSelfTo<ScoreManager>().AsSingle();
-
             Container.BindInterfacesAndSelfTo<AnalyticsAndAdsService>().AsSingle();
 
             bool useMobile = false;
